dd05e2c5194c60e5a5dfc17fd8ed28e217b3a8c6
goldzulu <goldzulu222@gmail.com>
2025-02-19 03:16:20 +0000

    abit more rebranding

--- apps/web/src/components/auth/signup/Signup.tsx
+++ apps/web/src/components/auth/signup/Signup.tsx
@@ -69,10 +69,10 @@ export function Signup() {
         <div className="absolute inset-0 bg-zinc-900" />
         <div className="relative z-20 flex gap-1 items-center text-lg font-medium">
           <NextImage
-            src="/lc_logo.jpg"
+            src="/xovox-black.svg"
             width={36}
             height={36}
-            alt="LangChain Logo"
+            alt="Xovox AI Logo"
             className="rounded-full"
           />
           Open Canvas
